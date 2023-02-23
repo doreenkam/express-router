0c1e1f4c119fcfec9c490d403ffd7fe9b5b7bf84
Doreen Kamushinda <dkamushinda@expediagroup.com>
2023-02-23 12:02:47 +0000

    Added routes for users & exported router

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+let users = [
+  {
+    name: 'User 1',
+    age: 30,
+  },
+  {
+    name: 'User 2',
+    age: 45,
+  },
+  {
+    name: 'User 3',
+    age: 27,
+  },
+  {
+    name: 'User 4',
+    age: 22,
+  },
+];
+
+router.get('/', (req, res) => {
+  try {
+    const data = users;
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+module.exports = router;
