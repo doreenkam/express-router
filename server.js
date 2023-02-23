8f6d6c2716d0cf68b39ef5aa16ee7a366b1b2cb5
Doreen Kamushinda <dkamushinda@expediagroup.com>
2023-02-23 12:03:10 +0000

    Added routes

--- server.js
+++ server.js
@@ -1,52 +1,12 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 5000;
+const usersRoutes = require('./routes/users');
+const fruitsRoutes = require('./routes/fruits');
 
-// List of Users
-let users = [
-    {
-        name: "User 1",
-        age: 30
-    },
-    {
-        name: "User 2",
-        age: 45
-    },
-    {
-        name: "User 3",
-        age: 27
-    },
-    {
-        name: "User 4",
-        age: 22
-    }
-]
-
-// List of Fruits
-let fruits = [
-    {
-        name: "Apple",
-        color: "Red"
-    },
-    {
-        name: "Banana",
-        color: "Yellow"
-    },
-    {
-        name: "Kiwi",
-        color: "Green"
-    },
-    {
-        name: "Grape",
-        color: "Purple"
-    },
-]
-
-// Express Routes
-
-
-
+app.use('/users', usersRoutes);
+app.use('/fruits', fruitsRoutes);
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
